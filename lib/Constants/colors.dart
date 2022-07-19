bcd97e1f390cf34d53755d35aec1c254848543a6
Laraib0000 <90485362+Laraib0000@users.noreply.github.com>
2022-07-19 12:59:35 +0500

    Delete lib directory

--- lib/Constants/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color primaryColor = Color(0xffaa80ff);
-const Color whiteC = Color(0xffffffff);
-const Color blackC = Color(0xff000000);
-const Color redC = Color(0xffff0000);
--- lib/Services/api_urls.dart
+++
@@ -1,4 +0,0 @@
-class AppUrl {
-  static const String baseUrl =
-      'https://volta-api.pea.co.th/v2/charger/item/13.8503779&100.5563737&17&true&true&true&true&true&true&true&true&true';
-}
